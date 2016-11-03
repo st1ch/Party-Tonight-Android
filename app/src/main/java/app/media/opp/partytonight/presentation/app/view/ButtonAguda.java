ea5b1a03464846440a293f41b7c89cc695ae1fd9
piekie <binnonnorie@gmail.com>
2016-11-03 23:11:29 +0200

    PROMOTER EVENTS ITEM added
    added layout of events item
    added "generic" java file for button with aguda font
    added "generic" java file for textView with aguda bold
    
    added dependency for rounded imageView to build.gradle file
    added mavenCentral to dependencies
    
    renamed textViewSign onto textViewAguda

--- app/src/main/java/app/media/opp/partytonight/presentation/app/view/ButtonAguda.java
+++ app/src/main/java/app/media/opp/partytonight/presentation/app/view/ButtonAguda.java
@@ -3,30 +3,30 @@ package app.media.opp.partytonight.presentation.app.view;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.widget.Button;
 
 /**
  * Created by piekie (Artem Vasylenko)
- * on 11/1/16
+ * on 11/3/16
  */
 
-public class TextViewSignHint extends TextView {
+public class ButtonAguda extends Button {
 
     private static final String FONT = "fonts/aguda.ttf";
 
-    public TextViewSignHint(Context context) {
+    public ButtonAguda(Context context) {
         super(context);
 
         configureView(context);
     }
 
-    public TextViewSignHint(Context context, AttributeSet attrs) {
+    public ButtonAguda(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         configureView(context);
     }
 
-    public TextViewSignHint(Context context, AttributeSet attrs, int defStyleAttr) {
+    public ButtonAguda(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
         configureView(context);
