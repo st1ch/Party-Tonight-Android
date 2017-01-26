27fc74878c958943131f8106917d422c9027ef7e
st1ch <artemg@tallium.com>
2017-01-26 17:27:14 +0200

    publishing

--- app/src/main/java/app/media/opp/partytonight/data/Constants.java
+++ app/src/main/java/app/media/opp/partytonight/data/Constants.java
@@ -4,7 +4,7 @@ package app.media.opp.partytonight.data;
  * Created by sebastian on 13.06.16.
  */
 public interface Constants {
-    String BASE_URL = "http://45.55.226.134:8080/partymaker/";
+    String BASE_URL = "http://45.55.226.134:8382/partymaker/";
 
 
 
