984421468a0b77e24bb14e88398a5c28996932d3
piekie <binnonnorie@gmail.com>
2017-03-27 21:59:21 +0300

    changed route on production version

--- app/src/main/java/app/media/opp/partytonight/data/Constants.java
+++ app/src/main/java/app/media/opp/partytonight/data/Constants.java
@@ -4,7 +4,7 @@ package app.media.opp.partytonight.data;
  * Created by sebastian on 13.06.16.
  */
 public interface Constants {
-    String BASE_URL = "http://10.0.2.2:8080/";
+    String BASE_URL = "http://45.55.226.134:8382/partymaker/";
 
 
 
